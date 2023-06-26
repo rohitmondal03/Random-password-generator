65cc701b4b9d98227bc91acc8ba225dd5c718323
Rohit <rohitmondall8000@gmail.com>
2023-06-26 11:05:34 +0530

    wrote logic of password checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,124 @@
 import React, { useRef, useState, useEffect } from 'react'
 import "./App.css"
 
+
+// FRAMER-MOTION
+import { motion } from 'framer-motion'
+
+
 // COMPONENTS
 import ProgressDemo from './components/ProgressBar'
+
 
 
 const App = () => {
   const passwordTextRef = useRef();
   const emailTextRef = useRef();
+
+  // FOR CHECKING THE CONDITIONS
   const uppercaseCheck = useRef();
   const numericCheck = useRef();
   const specialCharCheck = useRef();
+  const passwordLengthCheck = useRef();
 
   const [password, setPassword] = useState('')
   const [email, setEmail] = useState('')
+  const [progressBarLength, setProgressBarLength] = useState(0)
+  const [progressBarBackgroundColor, setProgressBarBackgroundColor] = useState('red')
+  const [submitButtonActive, setSubmitButtonActive] = useState(true)
+
+
+
+  // LIST ITEMS FOR CHECKING PASSWORD CONDITIONS 
+  const listItems = [
+    {
+      id: 1,
+      item: 'Should contain atleast one uppercase letter.',
+      ref: uppercaseCheck
+    },
+    {
+      id: 2,
+      item: 'Should contain atleast one or more number.',
+      ref: numericCheck
+    },
+    {
+      id: 3,
+      item: 'Should contain atleast one or more special chars',
+      ref: specialCharCheck
+    },
+    {
+      id: 4,
+      item: 'Should have greater that 10 characters.',
+      ref: passwordLengthCheck
+    },
+  ]
+
+
 
   useEffect(() => {
-    console.log(password);
+    const passwordLengthInstance = passwordTextRef.current.value.length
+
+    if (passwordLengthInstance === 0) {
+      setProgressBarLength(0)
+      setProgressBarBackgroundColor('red')
+    }
+    else if (passwordLengthInstance > 0 && passwordLengthInstance <= 2) {
+      setProgressBarLength(16.66666666)
+      setProgressBarBackgroundColor('red')
+    }
+    else if (passwordLengthInstance > 2 && passwordLengthInstance <= 4) {
+      setProgressBarLength(33.33333333)
+      setProgressBarBackgroundColor('rgb(221, 221, 0)')
+    }
+    else if (passwordLengthInstance > 4 && passwordLengthInstance <= 6) {
+      setProgressBarLength(50)
+      setProgressBarBackgroundColor('rgb(221, 221, 0)')
+    }
+    else if (passwordLengthInstance > 6 && passwordLengthInstance <= 8) {
+      setProgressBarLength(66.66666666)
+      setProgressBarBackgroundColor('rgb(138, 230, 0)')
+    }
+    else if (passwordLengthInstance > 8 && passwordLengthInstance <= 9) {
+      setProgressBarLength(83.33333333)
+      setProgressBarBackgroundColor('rgb(138, 200, 0)')
+    }
+    else if (passwordLengthInstance >= 10) {
+      setProgressBarLength(100)
+      setProgressBarBackgroundColor('rgb(0, 200, 0)')
+    }
   }, [password])
+
+
+
+  useEffect(function () {
+    // uppercaseCheck
+    passwordTextRef.current.value.match(/[A-Z]/)
+      ? uppercaseCheck.current.classList.add('text-teal-400', 'font-bold', 'text-lg')
+      : uppercaseCheck.current.classList.remove('text-teal-400', 'font-bold', 'text-lg')
+
+    // numericCheck
+    passwordTextRef.current.value.match(/[0-9]/)
+      ? numericCheck.current.classList.add('text-teal-400', 'font-bold', 'text-lg')
+      : numericCheck.current.classList.remove('text-teal-400', 'font-bold', 'text-lg')
+
+    // specialCharCheck
+    passwordTextRef.current.value.match(/[!, @, #, $, %, ^, &, *]/)
+      ? specialCharCheck.current.classList.add('text-teal-400', 'font-bold', 'text-lg')
+      : specialCharCheck.current.classList.remove('text-teal-400', 'font-bold', 'text-lg')
+
+    // passwordLengthCheck
+    passwordTextRef.current.value.length >= 10
+      ? passwordLengthCheck.current.classList.add('text-teal-400', 'font-bold', 'text-lg')
+      : passwordLengthCheck.current.classList.remove('text-teal-400', 'font-bold', 'text-lg')
+  }, [password])
+
+
 
   return (
     <div className='h-screen w-full relative'>
-      <div className='p-10 rounded-lg bg-pink-300 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-pink-500'>
+      <div className='p-[5rem] rounded-lg bg-pink-600 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-pink-500'>
 
-        <h1 className='font-bold text-3xl text-center'>Random password generator</h1>
+        <h1 className='font-bold text-3xl text-center text-zinc-100'>Random password generator</h1>
 
         <form className='mt-10 flex flex-col'>
 
@@ -51,44 +146,40 @@ const App = () => {
           />
 
           {/* --- STRENGTH SECTION --- */}
-          <div className='mb-10 mt-2'>
-            <h1 className='text-lg font-medium text-rose-700'>Password Strength:</h1>
-            <ul className='mt-2 mb-6 list-decimal'>
-              <li
-                ref={uppercaseCheck}
-              >
-                Should contain atleast one uppercase letter.
-              </li>
-
-              <li
-                ref={numericCheck}
-              >
-                Should contain atleast one or more number.
-              </li>
-
-              <li
-                ref={specialCharCheck}
-              >
-                Should contain atleast one or more special chars. ( * , / , ^ , & , $ , @ )
-              </li>
+          <div className='mb-10 mt-8'>
+            <h1 className='text-2xl font-medium text-white px-2'>Password Strength:</h1>
+            <ul className='my-2 list-decimal'>
+              {listItems.map((item => (
+                <li
+                  key={item.id}
+                  ref={item.ref}
+                  className='duration-500'
+                >
+                  {item.item}
+                </li>
+              )))}
             </ul>
-            <ProgressDemo />
+
+            <ProgressDemo
+              progressBarLength={progressBarLength}
+              progressBarBackgroundColor={progressBarBackgroundColor}
+            />
           </div>
 
           {/* --- BUTTONS --- */}
           <div className='mt-3 mx-auto flex flex-col justify-center items-center'>
-            <button
+            <motion.button
               text='Submit'
               type='submit'
-              className='px-5 py-1 rounded-lg bg-pink-600 text-white my-1'
+              className={`px-5 py-2 rounded-lg font-bold text-white my-1 transition duration-500 ease-out ${submitButtonActive? 'bg-blue-600 hover:scale-110 active:scale-95': 'bg-red-700'}`}
             >
               Submit
-            </button>
+            </motion.button>
 
             <button
               text="Generate random password"
               type='button'
-              className='px-5 py-1 rounded-lg bg-pink-600 text-white my-1'
+              className='px-5 py-1 rounded-lg bg-pink-200 font-bold text-black my-1'
             >
               Generate random password
             </button>
@@ -101,18 +192,6 @@ const App = () => {
   )
 }
 
-
-
-const Button = (props) => {
-  return (
-    <button
-      className='px-5 py-1 rounded-lg bg-pink-600 text-white my-1'
-      type={props.type}
-    >
-      {props.text}
-    </button>
-  )
-}
 
 
 export default App
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,24 +1,16 @@
-import React, { useEffect, useState } from 'react';
 import * as Progress from '@radix-ui/react-progress';
 
-import { motion } from 'framer-motion';
 
-
-const ProgressDemo = () => {
-    const [progress, setProgress] = useState(0);
-
-    useEffect(() => {
-        const timer = setTimeout(() => setProgress(66), 500);
-        return () => clearTimeout(timer);
-    }, []);
+const ProgressDemo = (props) => {
+    const { progressBarLength, progressBarBackgroundColor } = props;
 
     return (
         <Progress.Root
             className="relative overflow-hidden bg-zinc-200 rounded-full w-[400px] h-[20px] mx-auto"
-            value={progress}
         >
             <Progress.Indicator
-                className="bg-red-500 w-[20px] h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
+                className='bg-white w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]'
+                style={{ transition: ' 0.3s', transform: `translateX(-${100 - progressBarLength}%)`, backgroundColor: `${progressBarBackgroundColor}` }}
             />
         </Progress.Root>
     );
